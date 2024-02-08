a8ec11f18502b6ea6caf523560ca3e168fe5cb99
Caio Bianchi <caiohbs23@gmail.com>
2024-02-08 10:11:10 -0300

    added spring boot starter project

---
+++ src/test/java/com/caiohbs/restful/cinevault/CineVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiohbs.restful.cinevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
