05d12dc9cef3c9591beb9d1b9300df054f8f8e78
Caio Bianchi <caiohbs23@gmail.com>
2024-02-15 10:20:33 -0300

    changes in properties and added new repository method

--- src/main/java/com/caiohbs/restful/cinevault/movie/MovieRepository.java
+++ src/main/java/com/caiohbs/restful/cinevault/movie/MovieRepository.java
@@ -1,11 +1,15 @@
 package com.caiohbs.restful.cinevault.movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 
     Optional<Movie> findByOriginalTitle(String title);
+
+    List<Movie> findByEnglishTitleContaining(String title);
 
 }
